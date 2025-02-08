f29c66908f2233cdd9fb6dfff5edbd554cf216f9
Kdampil <wallycrism@gmail.com>
2025-02-07 20:56:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,13 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
 let mainWindow;
-let isDragging = false;
-let dragStartX = 0;
-let dragStartY = 0;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 400,
-    height: 300,
-    frame: false, // Borderless window
-    resizable: true, // Allow resizing
+    width: 800,
+    height: 600,
+    frame: false,
+    resizable: true,
     webPreferences: {
       preload: path.join(__dirname, 'renderer.js'),
       nodeIntegration: true,
@@ -19,27 +16,26 @@ function createWindow() {
     },
   });
 
-  // Enforce aspect ratio (e.g., 4:3)
+  mainWindow.loadFile('index.html');
   mainWindow.setAspectRatio(4 / 3);
 
-  mainWindow.loadFile('index.html');
-
-  // Make the window movable
-  mainWindow.webContents.on('did-finish-load', () => {
-    mainWindow.webContents.send('window-movable', true);
+  mainWindow.on('closed', () => {
+    mainWindow = null;
   });
-
-  mainWindow.on('closed', () => (mainWindow = null));
 }
 
 app.whenReady().then(() => {
   createWindow();
 
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
